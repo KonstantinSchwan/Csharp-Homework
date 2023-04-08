e4acc7c40eca90deba5bae22c8df3e69c4af05d6
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-08 22:26:17 +0200

    3 Задание готово

---
+++ HW3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Введите число : ");
+double a = double.Parse(Console.ReadLine());
+double b = a % 2;
+switch (b)
+{
+    case 0: System.Console.WriteLine("Число четное"); break;
+    default: System.Console.WriteLine("Число нечетное"); break;
+}
