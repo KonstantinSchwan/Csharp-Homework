8c2848a5865309b669caca16b8d3c505bffdf6ea
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-08 22:28:57 +0200

    4 Задание готово

---
+++ HW4/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+for (int b = 2; b <= a; b += 2)
+{
+    if (b < a - 1)
+        Console.Write(b + ", ");
+        else
+        Console.Write(b + ".");
+}
