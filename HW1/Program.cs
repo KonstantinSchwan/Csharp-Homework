df5e0df0d63a09cbec73df74be254195ad11c359
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-08 22:17:35 +0200

    1 Задание готово

---
+++ HW1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Максимальное число: " + Math.Max(a, b));
+Console.Write("Минимальное число: " + Math.Min(a, b));
